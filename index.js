156f0309e376e0c5616dfd00baee91f5487bffd4
Zack <zack@root-two.com>
2015-09-13 22:23:16 -0700

    fix blacklist variable, closes #1

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = function(persistor, config){
   function handleStorageEvent(e){
     if(e.key.indexOf(keyPrefix) === 0){
       var keyspace = e.key.substr(keyPrefix.length)
-      if(config.blacklist.indexOf(keyspace) === -1){
+      if(blacklist.indexOf(keyspace) === -1){
         persistor.rehydrate(keyspace, e.newValue, function(){
           //@TODO handle errors?
         })
