17d659ff06afe4428cd71baeed4c62d5f83ece51
Zack <zack@root-two.com>
2015-09-13 22:35:30 -0700

    add whitelist

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ var keyPrefix = constants.keyPrefix
 
 module.exports = function(persistor, config){
   var config = config || {}
-  var blacklist = config.blacklist || []
+  var blacklist = config.blacklist || false
+  var whitelist = config.whitelist || false
 
   window.addEventListener('storage', handleStorageEvent, false)
 
   function handleStorageEvent(e){
     if(e.key.indexOf(keyPrefix) === 0){
       var keyspace = e.key.substr(keyPrefix.length)
-      if(blacklist.indexOf(keyspace) === -1){
-        persistor.rehydrate(keyspace, e.newValue, function(){
-          //@TODO handle errors?
-        })
-      }
+      if(whitelist && whitelist.indexOf(keyspace) === -1){ return }
+      if(blacklist && blacklist.indexOf(keyspace) !== -1){ return }
+      
+      persistor.rehydrate(keyspace, e.newValue, function(){
+        //@TODO handle errors?
+      })
     }
   }
 }
