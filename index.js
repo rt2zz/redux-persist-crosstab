c2ff26247efc404e23ea63b1c3b8e5ecfc3cb12b
Kylie Rose <kylie.rose@temando.com>
2015-11-20 14:49:58 +1000

    cleaned up file

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-var constants = require('redux-persist/constants')
-var keyPrefix = constants.keyPrefix
+var constants = require('redux-persist/constants');
+var keyPrefix = constants.keyPrefix;
 
-module.exports = function(persistor, config){
-  var config = config || {}
-  var blacklist = config.blacklist || false
-  var whitelist = config.whitelist || false
+module.exports = function (persistor, config) {
+  var newConfig = config || {};
+  var blacklist = newConfig.blacklist || false;
+  var whitelist = newConfig.whitelist || false;
 
-  window.addEventListener('storage', handleStorageEvent, false)
+  window.addEventListener('storage', handleStorageEvent, false);
 
-  function handleStorageEvent(e){
-    if(e.key.indexOf(keyPrefix) === 0){
-      var keyspace = e.key.substr(keyPrefix.length)
-      if(whitelist && whitelist.indexOf(keyspace) === -1){ return }
-      if(blacklist && blacklist.indexOf(keyspace) !== -1){ return }
-      
-      persistor.rehydrate(keyspace, e.newValue, function(){
+  function handleStorageEvent(e) {
+    if (e.key.indexOf(keyPrefix) === 0) {
+      var keyspace = e.key.substr(keyPrefix.length);
+      if (whitelist && whitelist.indexOf(keyspace) === -1) { return; }
+      if (blacklist && blacklist.indexOf(keyspace) !== -1) { return; }
+
+      persistor.rehydrate(keyspace, e.newValue, function () {
         //@TODO handle errors?
       })
     }
