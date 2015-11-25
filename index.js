6c3f7104d24c91edbb1c83375ef9359983887244
Kylie Rose <kylie.rose@temando.com>
2015-11-25 11:30:27 +1000

    ensure localStorage is not fired in the source window with IE9/10 browsers

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 var constants = require('redux-persist/constants');
 var keyPrefix = constants.keyPrefix;
 
-module.exports = function (persistor, config) {
+module.exports = function(persistor, config) {
   var newConfig = config || {};
   var blacklist = newConfig.blacklist || false;
   var whitelist = newConfig.whitelist || false;
+  var isFocused = (document.hasFocus) ? document.hasFocus() : true;
 
-  window.addEventListener('storage', handleStorageEvent, false);
+  window.addEventListener('focus', function () {
+    isFocused = true;
+  }, false);
 
-  function handleStorageEvent(e) {
-    if (e.key.indexOf(keyPrefix) === 0) {
-      var keyspace = e.key.substr(keyPrefix.length);
+  window.addEventListener('blur', function () {
+    isFocused = false;
+  }, false);
+
+  window.addEventListener('storage', function (event) {
+    if (isFocused || !event.newValue) return;
+
+    if (event.key.indexOf(keyPrefix) === 0) {
+      var keyspace = event.key.substr(keyPrefix.length);
       if (whitelist && whitelist.indexOf(keyspace) === -1) { return; }
       if (blacklist && blacklist.indexOf(keyspace) !== -1) { return; }
 
-      persistor.rehydrate(keyspace, e.newValue, function () {
-        //@TODO handle errors?
-      })
+      persistor.rehydrate(keyspace, event.newValue, function (oldState, newState) {
+        // @TODO handle errors?
+      });
     }
-  }
+  }, false);
 }
