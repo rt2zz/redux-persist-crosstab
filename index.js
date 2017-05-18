cbf952f71d554c72913836bc26a8731a67ca2da2
Vlad Tsvang <vtsvang@gmail.com>
2017-05-18 16:59:06 -0700

    Makes keyPrefix configurable (#10)

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ module.exports = function(persistor, config){
   var config = config || {}
   var blacklist = config.blacklist || false
   var whitelist = config.whitelist || false
+  var keyPrefix = config.keyPrefix || KEY_PREFIX
 
   window.addEventListener('storage', handleStorageEvent, false)
 
   function handleStorageEvent(e){
-    if(e.key.indexOf(KEY_PREFIX) === 0){
-      var keyspace = e.key.substr(KEY_PREFIX.length)
+    if(e.key.indexOf(keyPrefix) === 0){
+      var keyspace = e.key.substr(keyPrefix.length)
       if(whitelist && whitelist.indexOf(keyspace) === -1){ return }
       if(blacklist && blacklist.indexOf(keyspace) !== -1){ return }
 
