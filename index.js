3dde594f11f434023e542a33855ead1a4c318ba6
Mark Allen <mark.ivan.allen@gmail.com>
2018-12-09 17:16:52 -0500

    v5 compat (#17)
    * initial v5 compat
    
    * fix whitelist and blacklist for reducer keys

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-var constants = require('redux-persist/constants')
-var KEY_PREFIX = constants.KEY_PREFIX
+// @flow
+import { KEY_PREFIX, REHYDRATE } from 'redux-persist/lib/constants'
+import type { PersistConfig } from 'redux-persist/es/types'
+import type { Store } from 'redux'
 
-module.exports = function(persistor, config){
-  var config = config || {}
-  var blacklist = config.blacklist || false
-  var whitelist = config.whitelist || false
-  var keyPrefix = config.keyPrefix || KEY_PREFIX
+type CrosstabConfig = {
+  blacklist?: ?Array<string>,
+  keyPrefix?: ?string,
+  whitelist?: ?Array<string>,
+}
+
+module.exports = function (store: Store, persistConfig: PersistConfig, crosstabConfig: CrosstabConfig = {}) {
+  const blacklist: ?Array<string> = crosstabConfig.blacklist || null
+  const whitelist: ?Array<string> = crosstabConfig.whitelist || null
+  const keyPrefix: string = crosstabConfig.keyPrefix || KEY_PREFIX
+
+  const { key }: { key: string } = persistConfig
 
   window.addEventListener('storage', handleStorageEvent, false)
 
-  function handleStorageEvent(e){
-    if(e.key && e.key.indexOf(keyPrefix) === 0){
-      var keyspace = e.key.substr(keyPrefix.length)
-      if(whitelist && whitelist.indexOf(keyspace) === -1){ return }
-      if(blacklist && blacklist.indexOf(keyspace) !== -1){ return }
-      if(e.oldValue === e.newValue){ return }
+  function handleStorageEvent (e) {
+    if (e.key && e.key.indexOf(keyPrefix) === 0) {
+      if (e.oldValue === e.newValue) {
+        return
+      }
 
-      var statePartial = {}
-      statePartial[keyspace] = e.newValue
-      persistor.rehydrate(statePartial, {serial: true})
+      const statePartial: { [string]: string } = JSON.parse(e.newValue)
+
+      /* eslint-disable flowtype/no-weak-types */
+      const state: Object = Object.keys(statePartial).reduce((state, reducerKey) => {
+        /* eslint-enable flowtype/no-weak-types */
+        if (whitelist && whitelist.indexOf(reducerKey) === -1) {
+          return state
+        }
+        if (blacklist && blacklist.indexOf(reducerKey) !== -1) {
+          return state
+        }
+
+        state[reducerKey] = JSON.parse(statePartial[reducerKey])
+
+        return state
+      }, {})
+
+      store.dispatch({
+        key,
+        payload: state,
+        type: REHYDRATE,
+      })
     }
   }
 }
